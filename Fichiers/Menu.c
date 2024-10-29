b31b8d4091c5ff04720ddb145c5963998530c475
Ibrahima BAH <ibrahima5.bah@etu.u-pec.fr>
2024-10-29 23:47:29 +0100

    Actualiser Fichiers/Menu.c

--- Fichiers/Menu.c
+++ Fichiers/Menu.c
@@ -8,6 +8,8 @@ int modeJeu = 0;
 int etatJeu = 1;  // 1 pour le menu, 2 pour le jeu
 int tourJoueur = 1;  // 1 pour joueur 1, 2 pour joueur 2
 int etapePlacement = 1;  // 1 pour placer la croix, 2 pour placer le pion
+int joueur1X, joueur1Y;
+int joueur2X, joueur2Y;
 
 int main(void) {
     int positionSourisX, positionSourisY;
@@ -26,77 +28,100 @@ int main(void) {
 
     int grilleAffichee = 0;
 
-    while (1) {
-        if (SourisCliquee()) {
-            SourisPosition();
-            positionSourisX = _X;
-            positionSourisY = _Y;
+   while (1) {
+    if (SourisCliquee()) {
+        SourisPosition();
+        positionSourisX = _X;
+        positionSourisY = _Y;
 
-            if (etatJeu == 1) {  // Mode Menu
-                gererInteractions(positionSourisX, positionSourisY);
-                afficherGrilleSiValidee(positionSourisX, positionSourisY);
+        if (etatJeu == 1) {  // Mode Menu
+            gererInteractions(positionSourisX, positionSourisY);
+            afficherGrilleSiValidee(positionSourisX, positionSourisY);
 
-                int boutonX = (LARGEUR_FENETRE - LARGEUR_BOUTON) / 2;
-                int boutonY = 400;
+            int boutonX = (LARGEUR_FENETRE - LARGEUR_BOUTON) / 2;
+            int boutonY = 400;
 
-                if (positionSourisX >= boutonX && positionSourisX <= boutonX + LARGEUR_BOUTON &&
-                    positionSourisY >= boutonY && positionSourisY <= boutonY + HAUTEUR_BOUTON) {
-                    
-                    if (choisirTaille >= 3 && choisirTaille <= 9 && modeJeu != 0) {
-                        EffacerEcran(CouleurParNom("white"));
+            if (positionSourisX >= boutonX && positionSourisX <= boutonX + LARGEUR_BOUTON &&
+                positionSourisY >= boutonY && positionSourisY <= boutonY + HAUTEUR_BOUTON) {
+                
+                if (choisirTaille >= 3 && choisirTaille <= 9 && modeJeu != 0) {
+                    EffacerEcran(CouleurParNom("white"));
 
-                        if (modeJeu == 1) {
-                            afficherFondJeu1();
-                        } else if (modeJeu == 2) {
-                            afficherFondJeu2();
-                        }
-
-                        initialiserJeu(choisirTaille);
-                        afficherGrilleJeu(choisirTaille);
-                        afficherMessageGraphique("Joueur 1 : Placez votre croix.");
-
-                        grilleAffichee = 1;
-                        etatJeu = 2;
-                        tourJoueur = 1;
-                        etapePlacement = 1;  // Commence avec le placement de la croix
-                    } else {
-                        afficherMessageGraphique("Veuillez choisir une taille et un mode de jeu.");
+                    if (modeJeu == 1) {
+                        afficherFondJeu1();
+                    } else if (modeJeu == 2) {
+                        afficherFondJeu2();
                     }
+
+                    initialiserJeu(choisirTaille);
+                    afficherGrilleJeu(choisirTaille);
+                    afficherMessageGraphique("Joueur 1 : Placez votre pion.");
+
+                    grilleAffichee = 1;
+                    etatJeu = 2;
+                    tourJoueur = 1;
+                    etapePlacement = 1;
+                } else {
+                    afficherMessageGraphique("Veuillez choisir une taille et un mode de jeu.");
                 }
-            } else if (etatJeu == 2) {  // Mode Jeu
-                if (tourJoueur == 1) {
-                    gererClicGrille(positionSourisX, positionSourisY, 1, choisirTaille);  // Appel pour joueur 1
-                    if (etapePlacement == 1) {
-                        afficherMessageGraphique("Joueur 1 : Placez votre pion.");
-                        etapePlacement = 2;  // Passe à l'étape de placement du pion
-                    } else if (etapePlacement == 2) {
-                        afficherMessageGraphique("Joueur 2 : Placez votre croix.");
-                        etapePlacement = 1;
-                        tourJoueur = 2;  // Change de joueur
-                    }
-                } else if (tourJoueur == 2) {
-                    gererClicGrille(positionSourisX, positionSourisY, 2, choisirTaille);  // Appel pour joueur 2
-                    if (etapePlacement == 1) {
-                        afficherMessageGraphique("Joueur 2 : Placez votre pion.");
-                        etapePlacement = 2;  // Passe à l'étape de placement du pion
-                    } else if (etapePlacement == 2) {
-                        afficherMessageGraphique("Joueur 1 : Placez votre croix.");
-                        etapePlacement = 1;
-                        tourJoueur = 1;  // Change de joueur
-                    }
+            }
+        } else if (etatJeu == 2) {  // Mode Jeu
+            if (tourJoueur == 1) {
+                if (etapePlacement == 1) {
+                    afficherMessageGraphique("Joueur 1 : Placez votre pion.");
+                    gererClicGrille(positionSourisX, positionSourisY, 1, choisirTaille);  // Place le pion
+
+                    // Passe à l'étape de placement de la croix
+                    etapePlacement = 2;
+                } else if (etapePlacement == 2) {
+                    afficherMessageGraphique("Joueur 1 : Placez votre croix.");
+                    gererClicGrille(positionSourisX, positionSourisY, 1, choisirTaille);  // Place la croix
+
+                    // Passe au tour du Joueur 2
+                    etapePlacement = 1;
+                    tourJoueur = 2;
+                }
+
+                // Vérifie si le joueur 1 est bloqué après son coup
+                if (estPartieTerminee(joueur1X, joueur1Y, choisirTaille)) {
+                    afficherScore(2);  // Si joueur 1 est bloqué, joueur 2 gagne
+                    break;  // Quitte la boucle pour afficher le score
+                }
+            } else if (tourJoueur == 2) {
+                if (etapePlacement == 1) {
+                    afficherMessageGraphique("Joueur 2 : Placez votre pion.");
+                    gererClicGrille(positionSourisX, positionSourisY, 2, choisirTaille);  // Place le pion
+
+                    // Passe à l'étape de placement de la croix
+                    etapePlacement = 2;
+                } else if (etapePlacement == 2) {
+                    afficherMessageGraphique("Joueur 2 : Placez votre croix.");
+                    gererClicGrille(positionSourisX, positionSourisY, 2, choisirTaille);  // Place la croix
+
+                    // Passe au tour du Joueur 1
+                    etapePlacement = 1;
+                    tourJoueur = 1;
+                }
+
+                // Vérifie si le joueur 2 est bloqué après son coup
+                if (estPartieTerminee(joueur2X, joueur2Y, choisirTaille)) {
+                    afficherScore(1);  // Si joueur 2 est bloqué, joueur 1 gagne
+                    break;  // Quitte la boucle pour afficher le score
                 }
             }
         }
-
-        if (Microsecondes() > suivant) {
-            suivant = Microsecondes() + CYCLE;
-        }
     }
 
-    libererSprites();
-    libererSpritesPions();
-    FermerGraphique();
-    return 0;
+    if (Microsecondes() > suivant) {
+        suivant = Microsecondes() + CYCLE;
+    }
+}
+
+// Libère les ressources et ferme le graphique une fois la partie terminée
+libererSprites();
+libererSpritesPions();
+FermerGraphique();
+return 0;
 }
 
 
