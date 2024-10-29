5b7ea2b4d3a1a09546e5eb7c39983ba335d9cfed
Ibrahima BAH <ibrahima5.bah@etu.u-pec.fr>
2024-10-29 23:53:28 +0100

    Actualiser Fichiers/Blocus.h

--- Fichiers/Blocus.h
+++ Fichiers/Blocus.h
@@ -13,7 +13,7 @@
 #define HAUTEUR_BOUTON 50        
 
 #define IMAGE_MENU "ImageMenu.jpg"   
-#define IMAGE_FOND1 "ImageFond1.jpg"
+#define IMAGE_FOND1 "ImageFond2.jpg"
 #define IMAGE_FOND2 "ImageFond2.jpg"  
 
 extern char grille[MAX_TAILLE][MAX_TAILLE]; 
