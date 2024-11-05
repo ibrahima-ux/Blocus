7d76cdbea2c00374cdcd046adcbf2b07efe74a53
Ibrahima BAH <ibrahima5.bah@etu.u-pec.fr>
2024-11-05 19:41:45 +0100

    Actualiser Fichiers/Recommencer.c

--- Fichiers/Recommencer.c
+++ Fichiers/Recommencer.c
@@ -1,22 +1,24 @@
 #include "Blocus.h"
+#include "BlocusAffichage.h"
 #include <graph.h>
 
 void revenirAuMenu() {
+    int i;
     EffacerEcran(CouleurParNom("white"));
 
-    // Réinitialise les variables de l'état de jeu
-    choisirTaille = 0;                // Réinitialise la taille sélectionnée
-    modeJeu = 0;                      // Réinitialise le mode de jeu
-    etatJeu = 1;                      // Passe en mode menu
+    
+    choisirTaille = 0;                
+    modeJeu = 0;                      
+    etatJeu = 1;                      
     etapePlacement = 1;
     tourJoueur = 1;
 
-    // Réinitialise toutes les cases des options de taille
-    for (int i = 0; i < MAX_OPTIONS; i++) {
-        casesCochees[i] = 0;          // Désélectionne toutes les options de taille
+    
+    for (i = 0; i < MAX_OPTIONS; i++) {
+        casesCochees[i] = 0;          
     }
 
-    // Affiche le menu principal
+
     afficherFondMenu();
-    TailleJeux();  // Affiche les options de taille de jeu
+    TailleJeux();  
 }
