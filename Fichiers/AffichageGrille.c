b59881e14ae221f064e6c287a726d7f632a40ac4
Ibrahima BAH <ibrahima5.bah@etu.u-pec.fr>
2024-11-05 19:26:46 +0100

    Actualiser Fichiers/AffichageGrille.c

--- Fichiers/AffichageGrille.c
+++ Fichiers/AffichageGrille.c
@@ -1,31 +1,36 @@
 #include "Blocus.h" 
+#include "BlocusAffichage.h"
 
 void afficherGrilleJeu(int taille) {
-    int cell_size = 50;
+    int cell_size = 50;  
     int totalWidth = taille * cell_size;
     int totalHeight = taille * cell_size;
 
     int x_start = (LARGEUR_FENETRE - totalWidth) / 2;
     int y_start = (HAUTEUR_FENETRE - totalHeight) / 2;
+    int border_thickness = 3;  // Épaisseur des bordures
 
-    int i, j;
+    int i, j,k;
     for (i = 0; i < taille; i++) {
         for (j = 0; j < taille; j++) {
-            // Affiche la couleur en fonction du contenu de la case
+            
             if (grille[i][j] == 'X') {
-                ChoisirCouleurDessin(CouleurParNom("grey"));  // Case bloquée
+                ChoisirCouleurDessin(CouleurParNom("grey")); 
             } else if (grille[i][j] == 'A') {
                 ChoisirCouleurDessin(CouleurParNom("red"));   // Joueur 1
             } else if (grille[i][j] == 'B') {
                 ChoisirCouleurDessin(CouleurParNom("blue"));  // Joueur 2
             } else {
-                ChoisirCouleurDessin(CouleurParNom("white")); // Case libre
+                ChoisirCouleurDessin(CouleurParNom("white")); 
             }
             RemplirRectangle(x_start + j * cell_size, y_start + i * cell_size, cell_size, cell_size);
 
-            // Dessiner le cadre
+            
             ChoisirCouleurDessin(CouleurParNom("black"));
-            DessinerRectangle(x_start + j * cell_size, y_start + i * cell_size, cell_size, cell_size);
+            for (k = 0; k < border_thickness; k++) {
+                DessinerRectangle(x_start + j * cell_size + k, y_start + i * cell_size + k,
+                                  cell_size - 2 * k, cell_size - 2 * k);
+            }
         }
     }
 }
