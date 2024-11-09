a9e3f2a0cc5a4825d4b529bcfa2c070ed62d8bc3
Ibrahima BAH <ibrahima5.bah@etu.u-pec.fr>
2024-11-09 19:42:52 +0100

    Ajouter Fichiers/Sprites.h

---
+++ Fichiers/Sprites.h
@@ -0,0 +1,16 @@
+#ifndef SPRITES_H
+#define SPRITES_H
+#include "constantes.h"
+#include <graph.h>
+extern int spriteCroixBleue;
+extern int spritePionBleu;
+extern int spriteCroixRouge;
+extern int spritePionRouge;
+
+
+
+void chargerSpritesPions();
+
+void libererSpritesPions();
+
+#endif  // SPRITES_H
