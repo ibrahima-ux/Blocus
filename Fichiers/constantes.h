1a45eeac0504b5f6e85a464536d018c18032cc1e
Ibrahima BAH <ibrahima5.bah@etu.u-pec.fr>
2024-11-09 19:43:55 +0100

    Ajouter Fichiers/constantes.h

---
+++ Fichiers/constantes.h
@@ -0,0 +1,29 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+// Définitions de constantes globales
+#define LARGEUR_FENETRE 800
+#define HAUTEUR_FENETRE 600
+#define LARGEUR_BOUTON 200
+#define HAUTEUR_BOUTON 50
+#define MAX_OPTIONS 7
+#define MAX_TAILLE 10
+
+#define CYCLE 10000L  
+
+// Variables globales externes (utilisées dans plusieurs fichiers)
+extern int choisirTaille;
+extern int modeJeu;
+extern int etatJeu;
+extern int etapePlacement;
+extern int tourJoueur;
+extern int casesCochees[MAX_OPTIONS];
+extern char grille[MAX_TAILLE][MAX_TAILLE];
+extern int joueur1X, joueur1Y;
+
+// Noms d'images
+#define IMAGE_MENU "ImageMenu.jpg"
+#define IMAGE_FOND1 "ImageFond1.jpg"
+#define IMAGE_FOND2 "ImageFond2.jpg"
+
+#endif
