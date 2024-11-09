e3c538ac9a940598b6fa85f2a29ed39e7468e42b
Ibrahima BAH <ibrahima5.bah@etu.u-pec.fr>
2024-11-09 19:43:25 +0100

    Ajouter Fichiers/main.c

---
+++ Fichiers/main.c
@@ -0,0 +1,172 @@
+#include <stdio.h>       
+#include <stdlib.h>      
+#include "Menu.h"
+#include "AffichageGrille.h"
+#include "CreationGrille.h"
+#include "MenuInteractions.h"
+#include "MenuOption.h"
+#include "Sprites.h"
+#include "Score.h"
+#include "Recommencer.h"
+#include "constantes.h"
+#include "MenuAffichage.h"  
+
+int main(void) {
+    int positionSourisX, positionSourisY;
+    unsigned long suivant = Microsecondes() + CYCLE;
+     int grilleAffichee = 0;
+       int boutonX, boutonY;
+      
+
+    // Initialisation graphique
+    if (InitialiserGraphique() == -1) {
+        fprintf(stderr, "Erreur lors de l'initialisation graphique.\n");
+        return EXIT_FAILURE;
+    }
+
+    // Création de la fenêtre
+    CreerFenetre(20, 20, LARGEUR_FENETRE, HAUTEUR_FENETRE);
+    chargerSprites();
+    chargerSpritesPions();
+    afficherFondMenu();
+    TailleJeux();
+
+   
+
+    // Boucle principale du jeu
+    while (1) {
+        if (SourisCliquee()) {
+            SourisPosition();
+            positionSourisX = _X;
+            positionSourisY = _Y;
+
+            // État de configuration du jeu (choix de taille et mode de jeu)
+            if (etatJeu == 1) {  
+                gererInteractions(positionSourisX, positionSourisY);
+                afficherGrilleSiValidee(positionSourisX, positionSourisY);
+
+                 boutonX = (LARGEUR_FENETRE - LARGEUR_BOUTON) / 2;
+                 boutonY = 400;
+
+                // Vérifie si les options de taille et de mode de jeu sont valides
+                if (positionSourisX >= boutonX && positionSourisX <= boutonX + LARGEUR_BOUTON &&
+                    positionSourisY >= boutonY && positionSourisY <= boutonY + HAUTEUR_BOUTON) {
+                    
+                    if (choisirTaille >= 3 && choisirTaille <= 9 && modeJeu != 0) {
+                        EffacerEcran(CouleurParNom("white"));
+
+                        if (modeJeu == 1) {
+                            afficherFondJeu1();
+                        } else if (modeJeu == 2) {
+                            afficherFondJeu2();
+                        }
+
+                        initialiserJeu(choisirTaille);
+                        afficherGrilleJeu(choisirTaille);
+                        afficherMessageGraphique("Joueur 1 : Placez votre pion.");
+
+                        grilleAffichee = 1;
+                        etatJeu = 2; 
+                        tourJoueur = 1;
+                        etapePlacement = 1;
+                    } else {
+                        afficherMessageGraphique("Veuillez choisir une taille et un mode de jeu.");
+                    }
+                }
+            } 
+            // Phase de jeu principale
+            else if (etatJeu == 2) {  
+                // Mode 1 joueur (Joueur contre IA) 
+                if (modeJeu == 1) {
+                    if (tourJoueur == 1) {  
+                        if (etapePlacement == 1) { 
+                            afficherMessageGraphique("Joueur 1 : Placez votre pion.");
+                            if (gererClicGrille(positionSourisX, positionSourisY, 1, choisirTaille)) {
+                                etapePlacement = 2;  
+                            }
+                        } else if (etapePlacement == 2) {  
+                            afficherMessageGraphique("Joueur 1 : Placez votre croix.");
+                            if (gererClicGrille(positionSourisX, positionSourisY, 1, choisirTaille)) {
+                                etapePlacement = 1;  
+                                tourJoueur = 2;
+                            }
+                        }
+
+                        if (estPartieTerminee(joueur1X, joueur1Y, choisirTaille)) {
+                            etatJeu = 3;      
+                            afficherScore(2);  
+                        }
+                    }
+                }
+                // Mode 2 joueurs (Humain contre humain) 
+                else if (modeJeu == 2) {
+                    if (tourJoueur == 1) {  
+                        if (etapePlacement == 1) {  
+                            afficherMessageGraphique("Joueur 1 : Placez votre pion.");
+                            if (gererClicGrille(positionSourisX, positionSourisY, 1, choisirTaille)) {
+                                etapePlacement = 2;  
+                            }
+                        } else if (etapePlacement == 2) {  
+                            afficherMessageGraphique("Joueur 1 : Placez votre croix.");
+                            if (gererClicGrille(positionSourisX, positionSourisY, 1, choisirTaille)) {
+                                etapePlacement = 1;  
+                                tourJoueur = 2;
+                            }
+                        }
+
+                        if (estPartieTerminee(joueur1X, joueur1Y, choisirTaille)) {
+                            etatJeu = 3;       
+                            afficherScore(2);  
+                        }
+                    } 
+                    else if (tourJoueur == 2) {  // Tour du Joueur 2
+                        if (etapePlacement == 1) {  
+                            afficherMessageGraphique("Joueur 2 : Placez votre pion.");
+                            if (gererClicGrille(positionSourisX, positionSourisY, 2, choisirTaille)) {
+                                etapePlacement = 2;  
+                            }
+                        } else if (etapePlacement == 2) {  
+                            afficherMessageGraphique("Joueur 2 : Placez votre croix.");
+                            if (gererClicGrille(positionSourisX, positionSourisY, 2, choisirTaille)) {
+                                etapePlacement = 1;  
+                                tourJoueur = 1;
+                            }
+                        }
+
+                        if (estPartieTerminee(joueur2X, joueur2Y, choisirTaille)) {
+                            etatJeu = 3;      
+                            afficherScore(1);  
+                        }
+                    }
+                }
+            }
+
+            // Tour du Joueur 2 (IA) sans clic pour le mode 1 joueur
+            if (etatJeu == 2 && modeJeu == 1 && tourJoueur == 2) {  
+                mouvementIA(&joueur2X, &joueur2Y, choisirTaille);  
+
+                if (estPartieTerminee(joueur2X, joueur2Y, choisirTaille)) {
+                    etatJeu = 3;      
+                    afficherScore(1);  
+                } else {
+                    tourJoueur = 1;  
+                }
+            }
+
+            // Gestion du mode fin de jeu avec retour au menu
+            if (etatJeu == 3) {
+                revenirAuMenu();  
+                etatJeu = 1;      
+            }
+        }
+
+        if (Microsecondes() > suivant) {
+            suivant = Microsecondes() + CYCLE;
+        }
+    }
+
+    libererSprites();
+    libererSpritesPions();
+    FermerGraphique();
+    return 0;
+}
