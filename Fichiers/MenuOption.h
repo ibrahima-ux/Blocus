ea88cdd6bd22a1724dc9a19407f0c6aca2ec6ddf
Ibrahima BAH <ibrahima5.bah@etu.u-pec.fr>
2024-11-09 19:39:49 +0100

    Ajouter Fichiers/MenuOption.h

---
+++ Fichiers/MenuOption.h
@@ -0,0 +1,14 @@
+#ifndef MENU_OPTION_H
+#define MENU_OPTION_H
+
+#include "constantes.h"
+
+void afficherOptionsJoueurs();
+
+void gererChoixJoueurs(int positionSourisX, int positionSourisY);
+
+void mouvementIA(int *x, int *y, int taille);
+
+int estPartieTerminee(int x, int y, int taille);
+
+#endif  // MENU_OPTION_H
